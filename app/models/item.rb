ceaa838e130e9c7f419aa9c6b3f5f768eb7b6924
Yousri <laifayousri@gmail.com>
2020-03-09 14:40:42 +0100

    add validates to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+    validates :title, presence: true, length: { in: 4..140 }
+    validates :description, presence: true, length: { in: 20..1000 }
+    validates :price, presence: true
+    validates :image_url, presence: true
 end
