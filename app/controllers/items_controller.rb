82b95164c50fe6be57d4e44b06911faa0c41fc59
Yousri <laifayousri@gmail.com>
2020-03-09 17:08:54 +0100

    edit items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,45 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    Item.find(params[:id]).update(item_params)
+
+    redirect_to item_path(@item)
+  end
+
+  def destroy
+    Item.find(params[:id]).destroy
+
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price)
   end
 end
