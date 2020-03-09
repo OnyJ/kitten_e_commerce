ccbfdda087f294a1f01f0d74ba35b895f2f068b6
Yousri <laifayousri@gmail.com>
2020-03-09 17:08:29 +0100

    edit validates to Itel model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,4 @@ class Item < ApplicationRecord
     validates :title, presence: true, length: { in: 4..140 }
     validates :description, presence: true, length: { in: 20..1000 }
     validates :price, presence: true
-    validates :image_url, presence: true
 end
