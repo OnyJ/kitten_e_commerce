cc6e7cf05229956a5869a328e7636715a460f723
OnyJ <julien.josselin75@hotmail.fr>
2020-03-10 18:07:50 +0100

    mailer - automatic send after user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_one :cart
 end
