eb9d320e1b8b4150cb7821baf4233ea25eed2887
OnyJ <julien.josselin75@hotmail.fr>
2020-03-10 15:04:14 +0100

    create model item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
     validates :title, presence: true, length: { in: 4..140 }
     validates :description, presence: true, length: { in: 20..1000 }
     validates :price, presence: true
+
+    has_many :line_items, dependent: :destroy
+    has_many :carts, through: :line_items
 end
