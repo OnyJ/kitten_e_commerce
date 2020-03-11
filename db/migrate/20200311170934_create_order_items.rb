384242faa5ceafdae5027ba4b1b32dcc17eb36b0
Yousri <laifayousri@gmail.com>
2020-03-11 18:18:40 +0100

    order_items table created

--- db/migrate/20200311170934_create_order_items.rb
+++ db/migrate/20200311170934_create_order_items.rb
@@ -1,7 +1,9 @@
 class CreateOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_items do |t|
-      t.integer :quantity
+      t.integer :quantity,   default: 1
+      t.belongs_to :order, index: true
+      t.belongs_to :item, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_170321) do
+ActiveRecord::Schema.define(version: 2020_03_11_170934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 2020_03_11_170321) do
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["item_id"], name: "index_line_items_on_item_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity", default: 1
+    t.bigint "order_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
