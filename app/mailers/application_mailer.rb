62d4e116053e15274a1b442fedc0b8eec4a51544
OnyJ <julien.josselin75@hotmail.fr>
2020-03-11 12:05:02 +0100

    edit user_mailer.rb to send welcome email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,14 +1,2 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
-
-  def welcome_email(user)
-    @user = user
-    # url we will use in the e-mail view
-    @url = 'https://kitten-pixtore-staging.herokuapp.com/'
-
-    # send and select the recipient
-    mail(to: @user.email, subject: 'Bienvenue sur Kitten Pixtore !')
-  end
-
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,14 @@
 class UserMailer < ApplicationMailer
-  default from: 'no-reply@monsite.fr'
- 
+  default from: 'from@example.com'
+  layout 'mailer'
+
   def welcome_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-    @user = user 
+    @user = user
+    # url we will use in the e-mail view
+    @url = 'https://kitten-pixtore-staging.herokuapp.com/'
 
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
-
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+    # send and select the recipient
+    mail(to: @user.email, subject: 'Bienvenue sur Kitten Pixtore !')
   end
+
 end
