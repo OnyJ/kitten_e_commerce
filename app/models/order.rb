6c46ea386ed14e3d18919c2ae25d3c61c12d18e4
OnyJ <julien.josselin75@hotmail.fr>
2020-03-12 17:01:08 +0100

    email - sold_send will send to every admin

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,23 @@
 class Order < ApplicationRecord
-    belongs_to :user
-    has_many :order_items, dependent: :destroy
-    has_many :items, through: :order_items
-
-    after_create :purchased_send
-    after_create :sold_send
+  belongs_to :user
+  has_many :order_items, dependent: :destroy
+  has_many :items, through: :order_items
   
-    def purchased_send
-      # Send an email to the user who bought the article
-      UserMailer.purchased_email(current_user).deliver_now
-    end
+  after_create :purchased_send
+  after_create :sold_send
   
-    def sold_send
-      # Send an email to the user who did create the article
-      UserMailer.sold_email(self).deliver_now
+  def purchased_send
+    # Send an email to the user who bought the article
+  # UserMailer.purchased_email(current_user).deliver_now
+    UserMailer.purchased_email(User.find(7)).deliver_now
+  end
+  
+  def sold_send
+    # Send an email to the user who did create the article
+    User.each do |user|
+      if user.profile.admin == true
+        UserMailer.sold_email(user).deliver_now
+      end
     end
+  end
 end
