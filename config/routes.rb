c9f1825eb1b34f63a94a4738ad60d13e6ded91b0
JeanBaptisteMigone <jbmigone@gmail.com>
2020-03-12 21:49:20 +0100

    url modifiées

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :items
-  resources :carts, only: [:show, :update, :destroy]
-  resources :orders, only: [:index, :create]
+  resources :items, path: "photo-de-chat"
+  resources :carts,only: [:show, :update, :destroy], path: "mon-panier"
+  resources :orders, only: [:index, :create], path: "paiement"
   
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
