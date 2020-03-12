fb47aedea17d0974064528a084eb81f020cdf6c5
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-03-12 12:00:25 +0100

    ajax front show cart + bug fixed line_item.destroy

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,7 +6,7 @@ class CartsController < ApplicationController
   def update
     @cart = current_user.cart
     @item = Item.find(params[:id])
-    @cart.items << @item
+    @line_item = LineItem.create(cart_id: @cart.id, item_id: @item.id)
 
     flash[:notice] = "L'article #{@item.title} à été ajouté au panier."
 
@@ -16,10 +16,14 @@ class CartsController < ApplicationController
   def destroy
     @cart = current_user.cart
     @item = Item.find(params[:id])
-    @item.destroy
-
+    @line_item = LineItem.find_by(cart_id: @cart.id, item_id: @item.id)
+    @line_item.destroy
+    
     flash[:notice] = "Votre article #{@item.title} à été supprimé du panier." if @cart.items.delete(@item)
 
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js { }
+    end
   end
 end
