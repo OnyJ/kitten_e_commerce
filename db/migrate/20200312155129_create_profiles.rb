ba20cb02430e6df75a9ae67fbf6c21c829d4d89a
OnyJ <julien.josselin75@hotmail.fr>
2020-03-12 18:23:12 +0100

    edit migration profile - set is_admin default: false

--- db/migrate/20200312155129_create_profiles.rb
+++ db/migrate/20200312155129_create_profiles.rb
@@ -1,7 +1,8 @@
 class CreateProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :profiles do |t|
-      t.boolean :admin
+      t.boolean :is_admin, default: false
+      t.belongs_to :user, index: true
 
       t.timestamps
     end
