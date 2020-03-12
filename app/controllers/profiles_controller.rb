d209e19bdd3c93554051d8ffd85c1b8468cd30d4
OnyJ <julien.josselin75@hotmail.fr>
2020-03-12 23:24:58 +0100

    add index method in profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,10 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
   before_action :check_privileges!, only: [:show]
 
+  def index
+    @orders = current_user.orders
+  end
+
   def show
     @orders = current_user.orders
   end
