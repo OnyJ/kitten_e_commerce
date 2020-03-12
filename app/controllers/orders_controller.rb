7ae9851ab63da9e3e68bd07e07c1d9ba90ca5870
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-03-12 18:04:08 +0100

    stripe working

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,19 +3,43 @@ class OrdersController < ApplicationController
     @orders = current_user.orders
   end
 
+  def new
+  end
+
   def create
-    @amount = current_user.cart.total_price.to_i * 100
+    @amount = current_user.cart.total_price.to_i
+
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: "Confirmation de la commande" ,
+      currency: 'eur',
+    })
 
     @order = Order.create(user_id: current_user.id, amount: @amount)
     current_user.cart.line_items.each do |line_item|
       OrderItem.create(order_id: @order.id, item_id: line_item.item.id, quantity: line_item.quantity)
     end
 
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to cart_path(current_user.cart)
+
     if @order.save
+      empty_cart
       flash[:notice] = "Your order was successfully taken"
       redirect_to root_path
     else
       redirect_to cart_path(current_user.cart)
     end
+  end
+
+  def empty_cart
+    current_user.cart.line_items.each(&:destroy)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items
   resources :carts, only: [:show, :update, :destroy]
-  resources :orders, only: [:index, :create]
+  resources :orders, only: [:index, :create, :new]
   
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
